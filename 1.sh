0be2ac8a82f2250497fdedadae725dee175f2342
Mehti <34041379+mr-mehti@users.noreply.github.com>
2022-10-23 05:19:49 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,16 +1,2 @@
 sudo chmod +x /root/openvpn/openvpn-install.sh && sudo chmod +x /root/openvpn/requirement.sh && sudo chmod +x /root/openvpn/status.sh && sudo /root/openvpn/requirement.sh
-cd /root/openvpn && ./openvpn-install.sh
-echo "[Unit]
-Description=Job that runs your user script
-
-[Service]
-ExecStart=nohup python3 -u /root/openvpn/app.py &> /root/openvpn/app.out &
-Type=simple
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target" >> /etc/systemd/system/mehti.service
-sudo systemctl daemon-reload
-sudo systemctl enable mehti.service
-sudo systemctl start mehti.service
-sudo systemctl restart mehti.service
+cd /root/openvpn && ./openvpn-install.sh && nohup python3 -u /root/openvpn/app.py &> /root/openvpn/app.out &
