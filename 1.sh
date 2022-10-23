6aed4394bec50e4b9edb9e262578bdd5dd17e897
Mehti <34041379+mr-mehti@users.noreply.github.com>
2022-10-23 05:36:28 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,5 @@
-sudo chmod +x /root/openvpn/openvpn-install.sh && sudo chmod +x /root/openvpn/requirement.sh && sudo chmod +x /root/openvpn/status.sh && sudo /root/openvpn/requirement.sh
+sudo chmod +x /root/openvpn/openvpn-install.sh
+sudo chmod +x /root/openvpn/requirement.sh
+sudo chmod +x /root/openvpn/status.sh
+sudo /root/openvpn/requirement.sh
 cd /root/openvpn && ./openvpn-install.sh && nohup python3 -u /root/openvpn/app.py &> /root/openvpn/app.out & && ls
