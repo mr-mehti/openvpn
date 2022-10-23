4e3f945b4d1cc9405a58dafb14e565545f42cb3b
Mehti <34041379+mr-mehti@users.noreply.github.com>
2022-10-23 06:40:10 -0700

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,4 @@
 #python3 /root/openvpn/app.py &
-nohup python3 /root/openvpn/app.py &
+#nohup python3 /root/openvpn/app.py &
+sudo bash -c '"$@" </dev/null >/dev/null 2>&1 & disown -h $!' _ \
+  python3 /root/openvpn/app.py
